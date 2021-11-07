05bfbba6485938fe3dbef54863baa6bc11c47df4
Georgi Parlakov <gparlakov@gmail.com>
2021-11-07 07:53:31 +0200

    WIP - add a typed workaround

--- src/app/forms-typed.ts
+++ src/app/forms-typed.ts
@@ -1,17 +1,19 @@
 import {
-  TypedFormGroup as OriginalTypedFormGroup,
+  Controls,
+  TypedFormGroup as _TypedFormGroup,
   typedFormGroup as originalTypedFormGroup,
 } from 'ngx-forms-typed';
 
 export {
-  OriginalTypedFormGroup,
-  originalTypedFormGroup,
   typedFormControl,
 } from 'ngx-forms-typed';
+
+export type OriginalTypedFormGroup<C, A extends Controls<K>> = _TypedFormGroup<C, A>;
+
 
 type NonGroup = string | number | boolean | Function | null | undefined | never;
 
 
-type TypedFormGroup = Except<OriginalTypedFormGroup, 'controls'> &{
+type TypedFormGroup = Except<_TypedFormGroup, 'controls'> & {
   controls: K extends NonGroup ? never : C;
-} 
+}
