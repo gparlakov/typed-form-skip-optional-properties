174c53344c62cbea4124a8a41783535aa42a9103
gparlakov <gparlakov@gmail.com>
2021-11-07 07:04:54 +0200

    WIP: add skip optional properties example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component, VERSION } from '@angular/core';
+import { typedFormGroup, originalTypedFormGroup } from 'ngx-forms-typed';
+
+type MyFormType = {
+  name: string;
+  optionalMiddleName?: string;
+};
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  form = typedFormGroup<MyFormType>({
+    name: typedFormControl(''),
+  });
+
+  formOriginal = originalTypedFormGroup<MyFormType>({
+    name: typedFormControl(''),
+    optionalMiddleName: typedFormControl(''),
+  });
+
+  ngOnInit() {
+    this.formOriginal.controls.optionalMiddleName.set('my middle name');
+
+    this.form.controls.optionalMiddleName.set({ withAFunction: () => {} });
+  }
 }
--- src/app/forms-typed.ts
+++ src/app/forms-typed.ts
@@ -1,3 +1,17 @@
-import { TypedFormGroup as OriginalTypedForm, typedFormGroup as originalTypedFormGroup } from 'ngx-forms-typed';
+import {
+  TypedFormGroup as OriginalTypedFormGroup,
+  typedFormGroup as originalTypedFormGroup,
+} from 'ngx-forms-typed';
 
-export * from 'ngx-forms-typed';
+export {
+  OriginalTypedFormGroup,
+  originalTypedFormGroup,
+  typedFormControl,
+} from 'ngx-forms-typed';
+
+type NonGroup = string | number | boolean | Function | null | undefined | never;
+
+
+type TypedFormGroup = Except<OriginalTypedFormGroup, 'controls'> &{
+  controls: K extends NonGroup ? never : C;
+} 
